826c5d5bad0101c0831a5e75e07a8739e7a39239
Mathilda <me@mathilda.dev>
2021-05-14 15:14:27 -0400

    Add variadic arguments to LogInfo

--- cli.go
+++ cli.go
@@ -5,7 +5,10 @@ import (
 	"time"
 )
 
-func LogInfo(message string) {
+func LogInfo(message string, args ...interface{}) {
 	dt := time.Now()
-	fmt.Printf("\033[36m%s \033[35m> \033[0m"+message+"\r\n", dt.Format(time.RFC1123Z))
+
+	fmt.Printf("\033[36m%s \033[35m> ", dt.Format(time.RFC1123Z))
+
+	fmt.Printf("\033[0m"+message+"\r\n", args...)
 }

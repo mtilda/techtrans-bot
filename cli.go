f456abdeb1f2a63df160bf657978532ea5e5e01d
Mathilda <me@mathilda.dev>
2021-05-14 15:23:12 -0400

    Add LogWarning

--- cli.go
+++ cli.go
@@ -13,6 +13,14 @@ func LogInfo(message string, args ...interface{}) {
 	fmt.Printf("\033[0m"+message+"\r\n", args...)
 }
 
+func LogWarning(message string, args ...interface{}) {
+	dt := time.Now()
+
+	fmt.Printf("\033[36m%s \033[35m> ", dt.Format(time.RFC1123Z))
+
+	fmt.Printf("\033[93mWARNING: "+message+"\r\n", args...)
+}
+
 func LogError(message string, args ...interface{}) {
 	dt := time.Now()
 

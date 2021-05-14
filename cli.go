05ca2aabfe5180ce25d91ac591a9df2e8311de0b
Mathilda <me@mathilda.dev>
2021-05-14 15:19:34 -0400

    Add LogError

--- cli.go
+++ cli.go
@@ -12,3 +12,11 @@ func LogInfo(message string, args ...interface{}) {
 
 	fmt.Printf("\033[0m"+message+"\r\n", args...)
 }
+
+func LogError(message string, args ...interface{}) {
+	dt := time.Now()
+
+	fmt.Printf("\033[36m%s \033[35m> ", dt.Format(time.RFC1123Z))
+
+	fmt.Printf("\033[1;31mERROR: "+message+"\r\n", args...)
+}

31de72979abf774e085e0e98762d2c4684af1252
Mathilda <me@mathilda.dev>
2021-05-17 05:01:59 -0400

    Reorganize IRC logic

--- bot.go
+++ bot.go
@@ -1,14 +1,9 @@
-// Provide tools for connecting to and interacting with Twitch IRC
+// Twitch IRC ChatBot
 
 package main
 
 import (
-	"bufio"
 	"errors"
-	"net"
-	"net/textproto"
-	"strings"
-	"time"
 )
 
 type Bot struct {
@@ -28,12 +23,13 @@ type Bot struct {
 	// Twitch OAuth credentials
 	Credentials OAuthCred
 
-	// Reference to the IRC connection
-	connection net.Conn
+	// IRC server controller
+	irc IRC
 }
 
-// Start bot
-// Connect to twitch, join channel, and handle chat
+/* Start bot
+   Connect to twitch, join channel, and handle chat
+*/
 func (bot *Bot) Start() {
 	err := bot.Credentials.Read(bot.CredentialsPath)
 	if nil != err {
@@ -42,72 +38,27 @@ func (bot *Bot) Start() {
 		return
 	}
 
-	bot.Connect()
-	defer bot.Disconnect()
+	bot.irc.Connect(bot.Server + ":" + bot.Port)
+	defer bot.irc.Disconnect(bot.Server + ":" + bot.Port)
 
 	bot.JoinChannel()
 
-	bot.Monitor()
+	bot.irc.Listen(bot)
 	if nil != err {
 		Error(err)
 	}
 }
 
-// Monitor the current IRC connection forever
-func (bot *Bot) Monitor() error {
-	tp := textproto.NewReader(bufio.NewReader(bot.connection))
-	for {
-		line, err := tp.ReadLine()
-		if err != nil {
-			Error(err)
-			return errors.New("Failed to read buffer")
-		}
+/* Respond to incoming messages
+ */
+func (bot *Bot) Chat(content string, sender string, isWhisper bool) error {
+	// bot.Say("Hello World")
 
-		formattedLine, err := bot.HandleIRCLine(line)
-		if err != nil {
-			Error(err)
-			return errors.New("Unable to parse IRC line: '" + line + "'")
-		}
-		if formattedLine != "" {
-			Inform(formattedLine)
-		}
-	}
+	return nil
 }
 
-// Parse a single IRC line
-// Check for commands
-// Format messages
-func (bot *Bot) HandleIRCLine(line string) (string, error) {
-	var formattedLine string
-
-	if strings.Contains(line, "PING :tmi.twitch.tv") {
-		_, err := bot.connection.Write([]byte("PONG :tmi.twitch.tv\r\n"))
-		if nil != err {
-			Error(err)
-			return "", errors.New("Unable to send 'PONG :tmi.twitch.tv'")
-		}
-
-		return "ping...pong", nil
-	}
-
-	lineSlice := strings.Split(line, " :")
-
-	if len(lineSlice) >= 1 {
-		user := strings.Split(lineSlice[0], " ")[0]
-		if strings.HasPrefix(user, ":") {
-			user = strings.Split(user, ":")[1]
-			user = strings.Split(user, "!")[0]
-			formattedLine += user + " : "
-		}
-	}
-	if len(lineSlice) >= 2 {
-		formattedLine += strings.Join(lineSlice[1:], " :")
-	}
-
-	return formattedLine, nil
-}
-
-// Make the bot send a message to the chat channel
+/* Send a message to the chat
+ */
 func (bot *Bot) Say(message string) error {
 	if message == "" {
 		return errors.New("Message empty")
@@ -118,18 +69,19 @@ func (bot *Bot) Say(message string) error {
 		return errors.New("Message exceeds 512 bytes")
 	}
 
-	_, err := bot.connection.Write([]byte("PRIVMSG #" + bot.Channel + " :" + message + "\r\n"))
+	err := bot.irc.Write("PRIVMSG #" + bot.Channel + " :" + message)
 	if nil != err {
 		return err
 	}
 
-	Inform(bot.Credentials.Nick + " : " + message)
+	Inform("(me): " + message)
 
 	return nil
 }
 
-// Make the bot send a whisper to a user
-// Bot must be verified in order for whispering to be allowed
+/* Send a whisper to a user
+   Bot must be verified in order for whispering to be allowed
+*/
 func (bot *Bot) Whisper(message string, recipient string) error {
 	if "" == message {
 		return errors.New("Message empty")
@@ -140,56 +92,26 @@ func (bot *Bot) Whisper(message string, recipient string) error {
 		return errors.New("Message exceeds 512 bytes")
 	}
 
-	_, err := bot.connection.Write([]byte("PRIVMSG #" + bot.Channel + " :/w " + recipient + " " + message + "\r\n"))
+	err := bot.irc.Write("PRIVMSG #" + bot.Channel + " :/w " + recipient + " " + message)
 	if nil != err {
 		return err
 	}
 
-	Inform(bot.Credentials.Nick + " \033[35m<whisper> " + recipient + " : " + message)
+	Inform("(I) \033[48:5:92;37m*whisper to (" + recipient + ")\033[0m: " + message)
 
 	return nil
 }
 
-// Connect the bot to the Twitch IRC server
-// Retry until successful, with exponential backoff
-func (bot *Bot) Connect() {
-	Inform("Attempting to connect with %s...", bot.Server)
+/* Join a channel
+   Will panic if connection is not established
+*/
+func (bot *Bot) JoinChannel() (err error) {
+	Inform("Joining #%s as @%s...", bot.Channel, bot.Credentials.Nick)
 
-	delay := 1
-	for {
-		// Make connection to Twitch IRC server
-		var err error
-		bot.connection, err = net.Dial("tcp", bot.Server+":"+bot.Port)
-		if err != nil {
-			Error(err)
-			Inform("Connection to %s failed, retrying in %d seconds...", bot.Server, delay)
-			time.Sleep(time.Duration(delay) * time.Second)
-			delay *= 2
-			continue
-		}
+	err = bot.irc.Write("PASS oauth:" + bot.Credentials.Password + "\r\n")
+	err = bot.irc.Write("NICK " + bot.Credentials.Nick + "\r\n")
+	err = bot.irc.Write("CAP REQ :twitch.tv/commands\r\n") // enable reading whispers
+	err = bot.irc.Write("JOIN #" + bot.Channel + "\r\n")
 
-		break
-	}
-
-	Inform("Connected to %s!", bot.Server)
-}
-
-// Disconnect bot from the Twitch IRC server
-func (bot *Bot) Disconnect() {
-	Inform("Disconnecting from IRC server.")
-	bot.connection.Close()
-	Inform("Closed connection with %s!", bot.Server)
-}
-
-// Make the bot join its pre-specified channel
-// Will panic if connection is not established
-func (bot *Bot) JoinChannel() {
-	Inform("Attempting to join #%s...", bot.Channel)
-
-	bot.connection.Write([]byte("PASS oauth:" + bot.Credentials.Password + "\r\n"))
-	bot.connection.Write([]byte("NICK " + bot.Credentials.Nick + "\r\n"))
-	bot.connection.Write([]byte("CAP REQ :twitch.tv/commands\r\n")) // enable reading whispers
-	bot.connection.Write([]byte("JOIN #" + bot.Channel + "\r\n"))
-
-	Inform("Joined #%s as @%s!", bot.Channel, bot.Credentials.Nick)
+	return
 }
---
+++ irc.go
@@ -0,0 +1,139 @@
+// Tools for interacting with the Twitch IRC server
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"net/textproto"
+	"strings"
+	"time"
+)
+
+type IRC struct {
+	// Address of IRC server
+	address string
+
+	// Reference to server connection
+	conn net.Conn
+}
+
+type Chatter interface {
+	Chat(content string, sender string, isWhisper bool) error
+}
+
+// Listen to current IRC connection forever
+func (irc *IRC) Listen(chatter Chatter) error {
+	tp := textproto.NewReader(bufio.NewReader(irc.conn))
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			Error(err)
+			return errors.New("Failed to read buffer")
+		}
+
+		err = irc.HandleLine(line, chatter)
+		if err != nil {
+			Error(err)
+			return errors.New("Unable to parse IRC line: '" + line + "'")
+		}
+	}
+}
+
+// Parse a single IRC line
+// Check for commands
+// Invoke chatter.Chat() on messages
+func (irc *IRC) HandleLine(line string, chatter Chatter) error {
+
+	// Avoid premature disconnections by playing ping-pong with server
+	if strings.Contains(line, "PING :tmi.twitch.tv") {
+		err := irc.Write("PONG :tmi.twitch.tv")
+		if nil != err {
+			Error(err)
+			return errors.New("Unable to send 'PONG :tmi.twitch.tv'")
+		}
+
+		Inform("ping...pong")
+	}
+
+	/* line is probably a message! */
+
+	// content comes after " :"
+	// prefix comes before " :"
+	ll := strings.Split(line, " :")
+
+	// if message has any content
+	if len(ll) >= 2 {
+		prefix, content, sender, isWhisper := ll[0], ll[1], "", false
+
+		// extract sender
+		sender = strings.Split(prefix, " ")[0]
+		if strings.HasPrefix(sender, ":") {
+			sender = strings.Split(sender, ":")[1]
+			sender = strings.Split(sender, "!")[0]
+		}
+
+		// check if message is a whisper
+		isWhisper = strings.Contains(prefix, "WHISPER")
+
+		if isWhisper {
+			Inform("(" + sender + ") \033[48:5:92;37m*whispers*\033[0m: " + content)
+		} else {
+			Inform("(" + sender + "): " + content)
+		}
+
+		// let the bot decide what it wants to do with this
+		err := chatter.Chat(content, sender, isWhisper)
+		if err != nil {
+			Error(err)
+		}
+	}
+
+	return nil
+}
+
+/* Write a line to the IRC server
+ */
+func (irc *IRC) Write(line string) (err error) {
+	_, err = irc.conn.Write([]byte(line))
+	if err != nil {
+		Error(err)
+		err = errors.New("Could not write line to IRC server: \"" + line + "\"")
+		return
+	}
+
+	return
+}
+
+/* Initiate connection to IRC server
+   Retry until successful, with exponential backoff
+*/
+func (irc *IRC) Connect(address string) {
+	Inform("Attempting to connect with %s...", address)
+
+	delay := 1
+	for {
+		// Make connection to Twitch IRC server
+		var err error
+		irc.conn, err = net.Dial("tcp", address)
+		if err != nil {
+			Error(err)
+			Inform("Connection to %s failed, retrying in %d seconds...", address, delay)
+			time.Sleep(time.Duration(delay) * time.Second)
+			delay *= 2
+			continue
+		}
+
+		break
+	}
+
+	Inform("Connected to %s!", address)
+}
+
+// Disconnect from IRC server
+func (irc *IRC) Disconnect(address string) {
+	Inform("Disconnecting from IRC server.")
+	irc.conn.Close()
+	Inform("Closed connection with %s!", address)
+}
